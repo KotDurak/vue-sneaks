257efc060803c9b40d37e4625e2d31af20243217
Maksim333 <DarkProkuror@mail.ru>
2024-02-25 09:42:58 +0400

    add vue course

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {reactive, ref} from "vue";
+const count = ref(0)
+
+const inc = () => {
+  count.value ++;
+}
+</script>
+
+<template>
+  <button @click="inc">+</button>
+  <p v-text="count"></p>
+</template>
+
+<style scoped>
+.red{
+  color:red;
+}
+</style>
